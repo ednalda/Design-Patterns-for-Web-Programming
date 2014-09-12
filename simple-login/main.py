14ea1daedb6fbbe55814db175edc6453b65551db
Ednalda <ednaldafakira@gmail.com>
2014-09-11 21:03:54 -0400

    space adjust - indentation

--- simple-login/main.py
+++ simple-login/main.py
@@ -24,30 +24,31 @@ import webapp2  # use the webapp2 library
 
 class MainHandler(webapp2.RequestHandler): #declaring a class
     def get(self):  #function that starts everything. Catalyst
-        page_head = '''<!DOCTYPE HTML>
-                      <html>
-                        <head>
-                            <title>Simple Form</title>
-                        </head>
-                        <body> '''
-                            page_body=''' <form method="GET" action="" >
-                               <h1>Register to post your Add</h1>
-                               <label>Name: </label><br/><input type="text" name="user" /><br />
-                                <label>Address: </label><br/><input type="text" name="address" /><br />
-                                <label>Phone: </label><br/><input type="text" name="phone" /><br /><br /><br />
-                                <label>Where do you want your add appear? </label><br />
-                                <input type="checkbox" name="orlando" value="orlando">Orlando<br />
-                                <input type="checkbox" name="miami" value="miami">Miami<br   />
-                                <label>Add </label><br/><input type="text" name="add" /><br /><br /><br />
-                                <label>Email: </label><br/><input type="text" name="email" /><br />
-                                <label>Password: </label><br/><input type="text" name="password" /><br /><br /><br />
-                                <a href="?email=marketing@add.com&user=marketing">Marketing Manager</a><br />
-                                <a href="?email=customer@add.com$user=customer">Customer Service</a><br /><br /><br />
-                                <input type="submit" value="Submit"/>'''
-                            page_close = '''
-                            </form>
-                        </body>
-                      </html>'''
+        page_head = ''' <!DOCTYPE HTML>
+                          <html>
+                             <head>
+                                <title>Simple Form</title>
+                             </head>
+                             <body> '''
+                               page_body=''' <form method="GET" action="" >
+                                                 <h1>Register to post your Add</h1>
+                                                 <label>Name: </label><br/><input type="text" name="user" /><br />
+                                                 <label>Address: </label><br/><input type="text" name="address" /><br />
+                                                 <label>Phone: </label><br/><input type="text" name="phone" /><br /><br /><br />
+                                                 <label>Where do you want your add appear? </label><br />
+                                                 <input type="checkbox" name="orlando" value="orlando">Orlando<br />
+                                                 <input type="checkbox" name="miami" value="miami">Miami<br   />
+                                                 <label>Add </label><br/><input type="text" name="add" /><br /><br /><br />
+                                                 <label>Email: </label><br/><input type="text" name="email" /><br />
+                                                 <label>Password: </label><br/><input type="text" name="password" /><br /><br /><br />
+                                                 <a href="?email=marketing@add.com&user=marketing">Marketing Manager</a><br />
+                                                 <a href="?email=customer@add.com$user=customer">Customer Service</a><br /><br /><br />
+                                                 <input type="submit" value="Submit"/>'''
+                               page_close = '''
+                               </form>
+                             </body>
+                          </html>'''
+
 
         if self.request.GET: #stablish condition
            user = self.request.GET['user']#condition true
