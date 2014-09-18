f56d1e29e8f8833b2d293c5876ab94b650b2a1fd
Ednalda <ednaldafakira@gmail.com>
2014-09-18 14:24:11 -0400

    test css

---
+++ encapsulation/main.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+'''
+name:Ednalda Fakira
+date:09/11/14
+class:Design Patterns for Web Programming - Online
+assignment: Simple Form
+'''
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        p = Page()
+        p.body = "you"
+        self.response.write(p.print_out())
+
+class Page(object):
+    def __init__(self):
+        self.title = "Welcome!"
+        self.css = "css/style.css"
+        self.head = """
+<!DOCTYPE HTML>
+ <html>
+   <head>
+          <title>{self.title}</title>
+          <link href="{self.css}" rel="stylesheet" type="text/css" />
+   </head>
+   <body>
+        """
+
+        self.body = "Hello!"
+        self.close = """
+
+   </body>
+ </html>
+       """
+
+    def print_out(self):
+        all = self.head + self.body + self.close
+        all = all.format(**locals())
+        return all
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
