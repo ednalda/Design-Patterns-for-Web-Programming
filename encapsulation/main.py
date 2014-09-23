a626aa4b2367057639bad51d94fafe0a84542727
Ednalda <ednaldafakira@gmail.com>
2014-09-23 14:05:11 -0400

    deleted htmls
    deleted html pages, create one class “Pages” for all htmls

--- encapsulation/main.py
+++ encapsulation/main.py
@@ -21,95 +21,133 @@ class:Design Patterns for Web Programming - Online
 assignment: Simple Form
 '''
 import webapp2
-
+from pages import Page #impor class Page from pages file
 class  MainHandler(webapp2.RequestHandler):
 
     def get(self):
+
+        s = Delivered()#next five objects
+        s.sale1 = 40
+        s.sale2 = 69
+        s.sale3 = 36
+        s.sale4 = 89
+        s.sale5 = 24
+        s.calc_total()
+        s_sales = [[s.sale1, s.sale2, s.sale3, s.sale4, s.sale5],[s.calc_total()]]
+
+
+        #November delivered
+        n = Delivered()
+        n.sale1 = 50
+        n.sale2 = 55
+        n.sale3 = 68
+        n.sale4 = 93
+        n.sale5 = 32
+        n.calc_total()
+        n_sales = [[n.sale1, n.sale2, n.sale3, n.sale4, n.sale5], [n.calc_total()]]
+
+
+
+        #February delivered
+        f = Delivered()
+        f.sale1 = 24
+        f.sale2 = 12
+        f.sale3 = 18
+        f.sale4 = 84
+        f.sale5 = 34
+        f.calc_total()
+        f_sales = [[f.sale1, f.sale2, f.sale3, f.sale4, f.sale5], [f.calc_total()]]
+
+
+        j = Delivered()
+        j.sale1 = 50
+        j.sale2 = 50
+        j.sale3 = 50
+        j.sale4 = 50
+        j.sale5 = 50
+        j.calc_total()
+        j_sales = [[j.sale1, j.sale2, j.sale3, j.sale4, j.sale5],[j.calc_total()]]
+
+
+        #May delivered
+        m = Delivered()
+        m.sale1 = 50
+        m.sale2 = 50
+        m.sale3 = 50
+        m.sale4 = 50
+        m.sale5 = 50
+        m.calc_total()
+        m_sales = [[m.sale1, m.sale2, m.sale3, m.sale4, m.sale5], [m.calc_total()]]
+
+
+        s_button = Button ()
+        s_button.label = "September"
+        s_button.result = s_sales[0][1]
+
+        n_button = Button ()
+        n_button.label = "November"
+        n_button.result =  n_sales[0][1]
+
+
+        f_button = Button ()
+        f_button.label = "February"
+        f_button.result = f_sales[0][1]
+
+
+        j_button = Button ()
+        j_button.label = "June"
+        j_button.result =  j_sales[0][1]
+
+        m_button = Button ()
+        m_button.label = "May"
+        m_button.result =  m_sales[0][1]
+
+
         p = Page()
         self.response.write (p.print_out())
 
 
 
-class Page(object):
+class Delivered(object):#class to call the attributes of objects.
+    def __init__(self): #Constrution method to design the object (Delivered)
+        self.sale1= 0
+        self.sale2= 0
+        self.sale3= 0
+        self.sale4= 0
+        self.sale5= 0
+        self.__total= 0 #private attribute only access inside this class
+
+
+ #decorators treating properties as variables
+    @property #getter: to return the total sales. It's accessing the total monthly sales attribute that is private only accessed inside the class Delivered.
+    def total(self):#function
+        return self.__total # return the total monthly sales attribute
+
+    @total.setter #setter: It gives the ability to update the total monthly sales result as it need.
+    def total(self, new_total):
+        self.__total = new_total
+
+    def calc_total(self):#function to calculate the total monthly sales by adding the sales together.
+        self.__total = self.sale1 + self.sale2 + self.sale3 + self.sale4 + self.sale5
+
+
+
+
+
+class Button (object):
     def __init__(self):
-        self.title = "Welcome!"
-        self.css = "css/style.css"
-        self.head = """
-<!DOCTYPE HTML>
- <html>
-   <head>
-          <title>{self.title}</title>
-          <link href="{self.css}" rel="stylesheet" type="text/css" />
-   </head>
-   <body>
-        """
+        self.label=""
+        self.result=""
 
-        self.body = '''<div id = "page">
-                               <header>
-                                    <nav>
-                                      <ul class="nav">
-                                          <li><img src="images/logo.jpg" class="logo" /></li>
-                                          <li><h1>Sweet Flower  Shop</h1></li>
-                                    </ul>
-                                          <ul class="sub_nav">
-                                             <li><a href="#">Home</a></li>
-                                             <li><a href="#">Orders</a></li>
-                                             <li><a href="#" class="active">Delivered</a></li>
-                                             <li><a href="#">Hot Deals</a></li>
-                                             <li><a href="#">Sign Out </a></li>
-                                          </ul>
-
-                                    </nav>
-                               </header>
-                               <div id = "content">
-                                    <article>
-                                      <ul>
-                                         <p><a href="?name=september"><input type="submit"  value= "September" class="submit" /></a></p>
-                                         <p><a href="?name=november"><input type="submit"  value= "November" class="submit" /></a></p>
-                                         <p><a href="?name=february" ><input type="submit" value= "February" class="submit" /></a></p>
-                                         <p><a href="?name=june" ><input type="submit"  value= "June" class="submit" /></a></p>
-                                         <p><a href="?name=may"><input type="submit"  value= "May" class="submit" /></a></p>
-                                      </ul>
-                                    </article>
-                                    <aside>
-                                    </aside>
-                               </div>
-                               '''
-
-        self.close = """
-                                <footer>
-                                  <p>Sweet Flower Shop @ 2014</p>
-                               </footer>
-                       </div>
-
-   </body>
- </html>
-       """
+    def show_result(self):#click button to call the object.
+        self.response.write = self.result
 
 
-    def  print_out(self):
-         all = self.head + self.body + self.close
-         all = all.format(**locals())
-         return all
 
 
-         if   self.request.GET:
-              september = self.request.GET['september.py']
-              self.response.write(self.head + self.body + self.close)
-         elif self.request.GET:
-              november = self.request.GET['november.py']
-              self.response.write(self.head + self.body + self.close)
-         elif self.request.GET:
-              february = self.request.GET['february.py']
-              self.response.write(self.head + self.body + self.close)
-         elif self.request.GET:
-              june = self.request.GET['june.py']
-              self.response.write(self.head + self.body + self.close)
-         elif self.request.GET:
-              may = self.request.GET['may.py']
-              self.response.write(self.head + self.body + self.close)
-         else:
-              self.response.write(self.head + self.body + self.close)
+
+
+
 
 
 
---
+++ encapsulation/pages.py
@@ -0,0 +1,169 @@
+__author__ = 'ednaldafakira'
+class Page(object):
+    def __init__(self):
+        self.title = "Welcome!"
+        self.css = "css/style.css"
+        self.head = """
+<!DOCTYPE HTML>
+ <html>
+   <head>
+          <title>{self.title}</title>
+          <link href="{self.css}" rel="stylesheet" type="text/css" />
+   </head>
+   <body>
+        """
+
+        self.body = '''<div id = "page">
+                               <header>
+                                    <nav>
+                                      <ul class="nav">
+                                          <li><img src="images/logo.jpg" class="logo" /></li>
+                                          <li><h1>Sweet Flower  Shop</h1></li>
+                                    </ul>
+                                          <ul class="sub_nav">
+                                             <li><a href="#">Home</a></li>
+                                             <li><a href="#">Orders</a></li>
+                                             <li><a href="#" class="active">Delivered</a></li>
+                                             <li><a href="#">Hot Deals</a></li>
+                                             <li><a href="#">Sign Out </a></li>
+                                          </ul>
+
+                                    </nav>
+                               </header>
+                               <div id = "content">
+                                    <article>
+                                     <form method="GET" action="">
+                                         <ul>
+                                            <p><a href="?name=september"><input type="submit"  value= "September" class="submit" /></a></p>
+                                            <p><a href="?name=november"><input type="submit"  value= "November" class="submit" /></a></p>
+                                            <p><a href="?name=february" ><input type="submit" value= "February" class="submit" /></a></p>
+                                            <p><a href="?name=june" ><input type="submit"  value= "June" class="submit" /></a></p>
+                                            <p><a href="?name=may"><input type="submit"  value= "May" class="submit" /></a></p>
+                                         </ul>
+                                      </form>
+                                    </article>
+                                    <aside>
+                                    </aside>
+                               </div>
+                               '''
+        self.september = ''' <div id = "content">
+                    <article>
+                    <h2>Your Monthly Expenses</h2>
+                          <ul>
+                               <h3>September</h3>
+                               <li>
+                                    <h3 id="s.sale1">Basket of Joy: </h3>
+                                    <h3 id="s.sale2">Country Basket Blooms: </h3>
+                                    <h3 id="s.sale3">Summer Brights: </h3>
+                                    <h3 id="s.sale4">Fashionista Blooms: </h3>
+                                    <h3 id="s.sale5">Garden Romance: </h3>
+                                    <h3 class="total">Total:</h3>
+                               </li>
+                          </ul>
+                    </article>
+           </div>'''
+
+        self.november = ''' <div id = "content">
+                                 <h2>Your Monthly Expenses</h2>
+                                   <article>
+                                        <ul>
+                                           <h3>November</h3>
+                                           <li>
+                                               <p>Cotton Candy: </p>
+                                               <p>Roses: </p>
+                                               <p>Tulips: </p>
+                                               <p>Daisies: </p>
+                                               <p>Gerberas: </p>
+                                           </li>
+                                        </ul>
+                                   </article>
+                        </div>'''
+
+
+
+        self.february = '''<div id = "content">
+                                   <h2>Your Monthly Expenses</h2>
+                                   <article>
+                                        <ul>
+                                             <h3>February</h3>
+                                             <li>
+                                               <p>Violeta: </p>
+                                               <p>Rose beauty: </p>
+                                               <p>Regal Orchids: </p>
+                                               <p>Yellow Trio Basket: </p>
+                                               <p>Sunny Cyclamen: </p>
+                                             </li>
+                                        </ul>
+                                   </article>
+                               </div>'''
+
+
+
+        self.june= '''<div id = "content">
+                                   <h2>Your Monthly Expenses</h2>
+                                   <article>
+                                        <ul>
+                                             <h3>June</h3>
+                                             <li>
+                                               <p>Country Basket Blooms: </p>
+                                               <p>Basket of Joy: </p>
+                                               <p>Regal Orchids: </p>
+                                               <p>Summer Brights: </p>
+                                               <p> Blooms: </p>
+                                             </li>
+                                        </ul>
+                                   </article>
+                        </div>'''
+
+
+        self.may = '''<div id = "content">
+                                            <h2>Your Monthly Expenses</h2>
+                                            <article>
+                                                 <ul>
+                                                    <h3>May</h3>
+                                                    <li>
+                                                         <p>Roses: </p>
+                                                         <p>Regal Orchids: </p>
+                                                         <p>Daisies: </p>
+                                                         <p>Tulips: </p>
+                                                         <p>Garden Romance: </p>
+                                                     </li>
+                                                 </ul>
+                                            </article>
+                      </div>'''
+
+        self.close = """
+                                <footer>
+                                  <p>Sweet Flower Shop @ 2014</p>
+                               </footer>
+                       </div>
+
+   </body>
+ </html>
+       """
+
+
+    def  print_out(self):
+         all = self.head + self.body + self.close
+         all = all.format(**locals())
+         return all
+
+
+
+         if   self.request.GET:
+              september = self.request.GET['s_button']
+              self.response.write(self.head + self.september + self.close)
+         elif self.request.GET:
+              november = self.request.GET['n_button']
+              self.response.write(self.head + self.november + self.close)
+         elif self.request.GET:
+              february = self.request.GET['f_button']
+              self.response.write(self.head + self.february + self.close)
+         elif self.request.GET:
+              june = self.request.GET['j_button']
+              self.response.write(self.head + self.june + self.close)
+         elif self.request.GET:
+              may = self.request.GET['m_button']
+              self.response.write(self.head + self.may + self.close)
+         else:
+              self.response.write(self.head + self.body + self.close)
