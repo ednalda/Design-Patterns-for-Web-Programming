b1b64d9d49a99912c80aeea1cfba537d0e0ba9de
Ednalda <ednaldafakira@gmail.com>
2014-10-06 13:28:56 -0400

    string

--- fakira_ednalda_Madlib/main.py
+++ fakira_ednalda_Madlib/main.py
@@ -4,3 +4,7 @@ last_name = "Fakira"
 print first_name + last_name #print variables to show complete name
 
 
+name = raw_input("Enter your last name")
+print name
+print "Hello ", name #print  message + variable that holds name
+
